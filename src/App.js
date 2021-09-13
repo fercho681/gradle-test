7b085940d5d7cadc245dd83fc98be7080c17eba1
fercho681 <85705344+fercho681@users.noreply.github.com>
2021-09-13 11:41:14 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-Your k8as deployments are fully automated!!!.        </p>
+Your kas deployments are fully automated!!!.        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
